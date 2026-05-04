de03d124aff36ce892143545094eca5570a2b254
Phylip Genty <phylipgenty@gmail.com>
2026-05-04 18:08:18 +0300

    Forward slash caramor

--- frontend/scripts/api.js
+++ frontend/scripts/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'https://wish-apps.onrender.com/';  // not localhost? Use 127.0.0.1
+const API_BASE = 'https://wish-apps.onrender.com';  // not localhost? Use 127.0.0.1
 
 let authToken = null;
 
